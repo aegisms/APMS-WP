c1b941375c38994e072c7149354bc6d38af1bba1
aegisms <aegisms2005@gmail.com>
2026-09-10 11:54:39 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-
 // https://vite.dev/config/
-// IMPORTANT: set `base` to "/<your-repo-name>/" before deploying to GitHub Pages,
-// e.g. base: "/activity-points-management-system/"
 export default defineConfig({
   plugins: [react()],
   base: "/APMS-WP/",
